e3c439689a6eee24aa774b54bcd03a48eaa71fd3
marcbayani <marc_bayani@rcc.edu.ph>
2024-03-09 15:38:06 +0800

    addition

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="math.php" method="POST">
+Input1: <input type="text" name="input1"><br>
+Input2: <input type="text" name="input2"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
---
+++ math.php
@@ -0,0 +1,8 @@
+<?php
+$input1 = $_POST["input1"];
+$input2 = $_POST["input2"];
+
+$output= 2;
+echo"The sum is:" . $output; 
+
+?>
